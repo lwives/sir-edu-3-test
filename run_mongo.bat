48f67d8563a1d82498514c743264094d79219f62
Edaurdo Amador Lucas <edualuc@gmail.com>
2018-07-18 20:43:14 -0300

    mensagem erro autenticação, usuário não encontrado.

--- run_mongo.bat
+++ run_mongo.bat
@@ -1,13 +1,20 @@
 @echo off
+IF "%2"=="repair" ( GOTO repair ) 
 IF "%1"=="trabalho" ( GOTO trabalho ) ELSE ( GOTO casa )
 
+:repair
+echo "repair"
+mongod --dbpath ..\data\ --repair
+goto fim
+
 :trabalho
+echo "trabalho"
 mongod --dbpath ..\data\ --storageEngine=mmapv1 
 goto fim
 
 :casa
-echo casa 
-mongod --dbpath ../data/ 
+echo "casa"
+mongod --dbpath ..\data\ 
 goto fim
 "%2"
 --storageEngine=wiredTiger 
