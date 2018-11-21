2e310f2015970ae5356522e713157f2e8e54547d
Eduardo Amador <eduardo_dna@trf4.jus.br>
2018-11-21 14:14:18 -0200

    Setar variavel MONGODB_URI

--- api/config/config.js
+++ api/config/config.js
@@ -2,7 +2,7 @@
 
 var config = {
   'secret': 'vouMeFormar',
-  'database': process.env.MONGOLAB_URI || 'mongodb://localhost:27017/SIR-EDU'
+  'database': process.env.MONGODB_URI || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/SIR-EDU'
 };
 
 module.exports = config;
